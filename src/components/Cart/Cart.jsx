53de127fcffc414cc8da2195277901c9ea6c0bab
Davide Briano <davidebriano@gmail.com>
2021-09-13 01:52:44 +0200

    add Firebase, implement Cart logic

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,11 +1,13 @@
 import classes from './cart.module.css';
 import Modal from '../UI/Modal';
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import { CartContext } from '../../store/CartProvider';
 import CartItem from './CartItem';
+import Checkout from './Checkout';
 
 const Cart = (props) => {
 	const cartCtx = useContext(CartContext);
+	const [isCheckout, setIsCheckout] = useState(false);
 
 	const totalAmount = '$' + cartCtx.totalAmount.toFixed(2);
 	const hasItems = cartCtx.items.length > 0;
@@ -17,6 +19,10 @@ const Cart = (props) => {
 	const cartItemAddHandler = (item) => {
 		cartCtx.addItem({ ...item, amount: 1 });
 	};
+
+	function orderHandler() {
+		setIsCheckout(true);
+	}
 
 	const cartItems = (
 		<ul className={classes['cart-items']}>
@@ -33,6 +39,19 @@ const Cart = (props) => {
 		</ul>
 	);
 
+	const modalActions = (
+		<div className={classes.actions}>
+			<button className={classes['button--alt']} onClick={props.onHideCart}>
+				Close
+			</button>
+			{hasItems && (
+				<button className={classes.button} onClick={orderHandler}>
+					Order
+				</button>
+			)}
+		</div>
+	);
+
 	return (
 		<Modal onClose={props.onHideCart}>
 			{cartItems}
@@ -40,12 +59,7 @@ const Cart = (props) => {
 				<span> Total Amount</span>
 				<span>{totalAmount}</span>
 			</div>
-			<div className={classes.actions}>
-				<button className={classes['button--alt']} onClick={props.onHideCart}>
-					Close
-				</button>
-				{hasItems && <button className={classes.button}>Order</button>}
-			</div>
+			{isCheckout ? <Checkout onCancel={props.onHideCart} /> : modalActions}
 		</Modal>
 	);
 };
--- src/components/Meals/AvailableMeals.jsx
+++ src/components/Meals/AvailableMeals.jsx
@@ -1,36 +1,46 @@
+import { useEffect, useState } from 'react';
 import classes from './AvailableMeals.module.css';
 import Card from '../UI/Card';
 import MealItem from './MealItem/MealItem';
 
-const DUMMY_MEALS = [
-	{
-		id: 'm1',
-		name: 'Sushi',
-		description: 'Finest fish and veggies',
-		price: 22.99,
-	},
-	{
-		id: 'm2',
-		name: 'Schnitzel',
-		description: 'A german specialty!',
-		price: 16.5,
-	},
-	{
-		id: 'm3',
-		name: 'Barbecue Burger',
-		description: 'American, raw, meaty',
-		price: 12.99,
-	},
-	{
-		id: 'm4',
-		name: 'Green Bowl',
-		description: 'Healthy...and green...',
-		price: 18.99,
-	},
-];
-
 function AvailableMeals() {
-	const mealsList = DUMMY_MEALS.map((meal) => (
+	const [meals, setMeals] = useState([]);
+	const [isLoading, setIsLoading] = useState(true);
+	const [hasError, setHasError] = useState(null);
+
+	useEffect(() => {
+		async function fetchMeals() {
+			setIsLoading(true);
+			const response = await fetch(
+				'https://react-food-df3ce-default-rtdb.europe-west1.firebasedatabase.app/meals.json'
+			);
+			if (!response.ok) throw new Error('Something went wrong!');
+			const responseData = await response.json();
+
+			const loadedMeals = [];
+
+			for (const key in responseData) {
+				loadedMeals.push({
+					id: key,
+					name: responseData[key].name,
+					description: responseData[key].description,
+					price: responseData[key].price,
+				});
+			}
+			console.log(loadedMeals);
+			setMeals(loadedMeals);
+			setIsLoading(false);
+		}
+
+		console.log(
+			fetchMeals().catch((error) => setHasError(error.message), void setIsLoading(false))
+		);
+	}, []);
+
+	if (isLoading) return <section className={classes.MealsLoading}>Loading...</section>;
+	if (hasError) return <section className={classes.MealsError}>{hasError}</section>;
+
+	const mealsList = meals.map((meal) => (
 		<MealItem
 			key={meal.id}
 			id={meal.id}
